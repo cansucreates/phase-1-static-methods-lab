4a0c8b2bd188dae9f1437e497093e4e2b17e2c8e
Cansu Aysagdic <cansu.aysagdic@ogr.iu.edu.tr>
2023-04-05 14:09:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+static capitalize(string){
+ return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+static sanitize(string){
+  const nonAlphanumericRegex = /[^a-zA-Z0-9-' ]/g;
+  return string.replace(nonAlphanumericRegex, '');
+}
+static titleize(string){
+  const exceptions = ['the','a','an','but','of','and','for','at','by','from'];
+  const words = string.split(' ');
+  const titleizedWords = words.map((word,index) => {
+    if (index === 0 || !exceptions.includes(word)) {
+      return this.capitalize(word);
+    }
+    else {
+      return word;
+    }
+  });
+
+  return titleizedWords.join(' ');
+
+}
 }
